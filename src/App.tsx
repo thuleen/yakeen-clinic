eaf63f6d7067c4c3f525f7f9cf42d95557b6c9d4
Azlan Abdul Aziz <azlan@Azlans-MacBook-Air.local>
2022-06-27 17:43:12 +0800

    Clinic Azlan Aziz

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Yakeen Clinic Azlan</p>
+        <p>Yakeen Clinic Azlan Aziz</p>
       </header>
     </div>
   )
