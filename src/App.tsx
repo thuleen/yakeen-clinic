581bbae7ffda6bbd276a64cad13d7453de3559ea
Azlan Abdul Aziz <azlan@Azlans-MacBook-Air.local>
2022-06-27 17:33:18 +0800

    Try change 2

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Yakeen</p>
+        <p>Yakeen Clinic</p>
       </header>
     </div>
   )
