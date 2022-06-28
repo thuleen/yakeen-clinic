9d26e1fee876470513843304869db48fd1824aa2
Azlan Abdul Aziz <azlan@Azlans-MacBook-Air.local>
2022-06-28 11:50:52 +0800

    Adding gh-pages

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <BrowserRouter basename="https://thuleen.github.io/yaqeen-clinic/">
+      <BrowserRouter>
         <App />
       </BrowserRouter>
     </ThemeProvider>
